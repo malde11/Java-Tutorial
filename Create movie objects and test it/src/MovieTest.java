f3f80f9507612e863388da7d4fc526878442d7a2
test user <test.user@test.com>
2018-08-22 00:44:03 +0200

    Create movie test class

--- Create movie objects and test it/src/MovieTest.java
+++ Create movie objects and test it/src/MovieTest.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class MovieTest {
+
+    public static void main(String[] args) {
+
+        Movie one = new Movie();
+        one.title   = "Von der Krise verweht";
+        one.genre   = "Tragisch";
+        one.rating  = -2;
+
+        Movie two = new Movie();
+        two.title   = "Vier Deadlines und ein Todesfall";
+        two.genre   = "Komödie";
+        two.rating  = 5;
+        two.showMovie();
+
+        Movie three = new Movie();
+        three.title     = "Byte Club";
+        three.genre     = "Tragisch, aber letzendlich aufbauend";
+        three.rating    = 127;
+
+    }
+
+
 }
