65481f62ef4dcb00d3abf48c896378238c3a4a3d
test user <test.user@test.com>
2018-08-22 05:43:40 +0200

    Added main function

--- easyDotCom/src/EasyDotCom.java
+++ easyDotCom/src/EasyDotCom.java
@@ -1,13 +1,44 @@
 public class EasyDotCom {
 
-    int[] celllocation;
-    int numberofHits;
+  public static void main(String[] args) {
 
-    public void setCelllocation(int[] location) {
+      int numberofHits = 0;
+
+      GameHelper helper = new GameHelper();
+      EasyDotCom theDotCom = new EasyDotCom();
+
+      int randomNumber = (int) (Math.random() * 5);
+      int[] location = {randomNumber, randomNumber + 1, randomNumber + 2};
+
+      theDotCom.setCelllocation(location);
+      boolean alive = true;
+
+      while (alive == true) {
+
+          String tipp = helper.getUserInsert("Geben Sie eine Zahl ein: ");
+          String result = theDotCom.checkMe(tipp);
+
+          numberofHits++;
+
+          if (result.equals("Killed!")) {
+
+              alive = false;
+
+              System.out.println("Sie haben " + numberofHits + " Versuche benötigt.");
+          }
+
+      }
+
+  }
+
+  int[] celllocation;
+  int numberofHits;
+
+  public void setCelllocation(int[] location) {
         celllocation = location;
-    }
+  }
 
-    public String checkMe(String stringTipp) {
+  public String checkMe(String stringTipp) {
 
         int tipp = Integer.parseInt(stringTipp);
         String result = "No Hit!";
@@ -32,11 +63,6 @@ public class EasyDotCom {
 
 
         return result;
-    }
-
-
-
-
-
+  }
 
 }
