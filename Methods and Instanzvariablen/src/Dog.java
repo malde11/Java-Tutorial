9bd542e2b99af0d10ba8100a456fee1c1a6a9a32
test user <test.user@test.com>
2018-08-22 02:48:45 +0200

    Create Dog Class

---
+++ Methods and Instanzvariablen/src/Dog.java
@@ -0,0 +1,27 @@
+public class Dog {
+
+    int      size;
+    String   name;
+
+    void bark() {
+
+        if (size > 60) {
+
+            System.out.println("Wuff Wuff!");
+
+        } else if (size > 14) {
+
+            System.out.println("Wau Wau!");
+
+        } else {
+
+            System.out.println("Jip Jip!");
+
+        }
+
+
+
+    }
+
+
+}
