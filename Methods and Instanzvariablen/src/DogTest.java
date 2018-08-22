3d7994e070ae0daead26b45bc4b842dc8e786f67
test user <test.user@test.com>
2018-08-22 02:52:17 +0200

    Create DogTest Class

---
+++ Methods and Instanzvariablen/src/DogTest.java
@@ -0,0 +1,23 @@
+public class DogTest {
+
+    public static void main(String[] args) {
+
+        Dog d1 = new Dog();
+        d1.size = 70;
+
+        Dog d2 = new Dog();
+        d2.size = 8;
+
+        Dog d3 = new Dog();
+        d3.size = 35;
+
+        d1.bark();
+        d2.bark();
+        d3.bark();
+
+
+    }
+
+
+
+}
