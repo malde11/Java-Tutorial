01bc57eb01672250aee5feea7383704cece49ebe
test user <test.user@test.com>
2018-08-22 04:50:35 +0200

    Updated Test

---
+++ easyDotCom/src/EasyDotComTest.java
@@ -0,0 +1,20 @@
+public class EasyDotComTest {
+
+    public static void main(String[] args) {
+
+        EasyDotCom dotCom = new EasyDotCom();
+
+        int[] location = {2,3,4};
+
+        dotCom.setCelllocation(location);
+
+        String tipp = "2";
+
+        String result = dotCom.checkMe(tipp);
+
+
+    }
+
+
+
+}
